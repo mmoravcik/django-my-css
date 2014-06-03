50483911480d54a6678140154d5eb5e4c68cfae9
Matus Moravcik <matus.moravcik@gmail.com>
2014-06-03 20:17:54 +0100

    Add archive to admin

--- my_css/admin.py
+++ my_css/admin.py
@@ -2,6 +2,8 @@ from django.db.models import get_model
 from django.contrib import admin
 
 MyCSS = get_model('my_css', 'MyCSS')
+MyCSSArchive = get_model('my_css', 'MyCSSArchive')
 
 
 admin.site.register(MyCSS)
+admin.site.register(MyCSSArchive)
